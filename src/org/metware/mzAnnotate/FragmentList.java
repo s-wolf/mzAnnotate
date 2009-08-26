9016d38a9397b52d1a73d92561022df31551d183
sebastianwolf <sebastianwolf@843ea7cd-573f-0410-892e-8e620655647f>
2009-08-26 12:10:22 +0000

    List of fragments
    git-svn-id: https://metware.svn.sourceforge.net/svnroot/metware/mzAnnot/trunk@990 843ea7cd-573f-0410-892e-8e620655647f

---
+++ src/org/metware/mzAnnotate/FragmentList.java
@@ -0,0 +1,110 @@
+package org.metware.mzAnnotate;
+
+import java.util.HashMap;
+
+import org.openscience.cdk.interfaces.IAtomContainer;
+import org.openscience.cdk.interfaces.IMolecularFormula;
+import org.openscience.cdk.tools.manipulator.MolecularFormulaManipulator;
+
+public class FragmentList {
+	
+	private HashMap<String, Fragment> fragMap;
+	private int idCount = 0;
+	
+	/**
+	 * Instantiates a new fragment list.
+	 */
+	public FragmentList()
+	{
+		this.fragMap = new HashMap<String, Fragment>();
+	}
+	
+	
+	/**
+	 * Adds the fragment and returns the id from it. With this id it is possible 
+	 * to connect a peak and this fragment later on.
+	 * 
+	 * @param mol the mol
+	 * 
+	 * @return the string
+	 */
+	public String addFragment(IAtomContainer mol)
+	{
+		IMolecularFormula molFormula = MolecularFormulaManipulator.getMolecularFormula(mol);
+		String id = "f" + idCount;
+		Fragment frag = new Fragment(molFormula, false);
+		fragMap.put(id, frag);
+		idCount++;
+		return id;
+	}
+	
+	/**
+	 * Adds the fragment and returns the id from it. With this id it is possible 
+	 * to connect a peak and this fragment later on. Only the molecular formula is known.
+	 * 
+	 * @param mol the mol
+	 * 
+	 * @return the string
+	 */
+	public String addFragment(IMolecularFormula formula)
+	{
+		String id = "f" + idCount;
+		Fragment frag = new Fragment(formula, false);
+		fragMap.put(id, frag);
+		idCount++;
+		return id;
+	}
+	
+	/**
+	 * Adds the measured structure.
+	 * 
+	 * @param mol the mol
+	 * 
+	 * @return the string
+	 */
+	public String addStructure(IAtomContainer mol)
+	{
+		IMolecularFormula molFormula = MolecularFormulaManipulator.getMolecularFormula(mol);
+		String id = "m" + idCount;
+		Fragment frag = new Fragment(molFormula, true);
+		fragMap.put(id, frag);
+		idCount++;
+		return id;
+	}
+	
+	/**
+	 * Adds the measured structure but only the molecular formula. The structure is not known.
+	 * 
+	 * @param mol the mol
+	 * 
+	 * @return the string
+	 */
+	public String addStructure(IMolecularFormula formula)
+	{
+		String id = "m" + idCount;
+		Fragment frag = new Fragment(formula, true);
+		fragMap.put(id, frag);
+		idCount++;
+		return id;
+	}
+	
+
+	/**
+	 * Sets the frag map
+	 * 
+	 * @param fragList the new frag map
+	 */
+	public void setFragList(HashMap<String, Fragment> fragList) {
+		this.fragMap = fragList;
+	}
+
+	/**
+	 * Gets the frag map
+	 * 
+	 * @return the frag map
+	 */
+	public HashMap<String, Fragment> getFragList() {
+		return fragMap;
+	}
+
+}
